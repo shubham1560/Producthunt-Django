00d84f8634d36d6edf7a5175a1045b0acde9e337
Shubham <shubhamsinha2050@gmail.com>
2019-09-10 19:58:24 +0530

    Always add apps to the setting and make proper migrations of the model

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products.apps.ProductsConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
 
 
 # Internationalization
