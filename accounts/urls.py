664c6cb2c0289b49cc12df853032ffba4694c5f0
Shubham <shubhamsinha2050@gmail.com>
2019-09-11 23:05:55 +0530

    Added the urls to manage users

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from products import views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", views.home, name="home"),
-    path("accounts/", include('accounts.urls'))
+    path("", views.login, name = "login"),
+    path("logout", views.logout, name = "logout"),
+    path("signup", views.signUp, name="signup")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def login(request):
+    return render(request, 'Login.html')
+
+
+def signUp(request):
+    return render(request, "SignUp.html")
+
+
+def logout(request):
+    return render(request, "Logout.html")
