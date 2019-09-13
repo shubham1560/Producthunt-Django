4794cf433431b0a2748d9a8fad82ec8c4db14805
Shubham <shubhamsinha2050@gmail.com>
2019-09-13 22:14:26 +0530

    Added the products model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    pub_date = models.DateField()
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    votes_total = models.IntegerField(default=1)
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title, self.url
+
